034147a07ec7ddad26134ac0bf16d3dee4a7621a
stanHua <stanhua@aliyun.com>
2016-10-20 22:14:53 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,41 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            //压缩（minify）JavaScript文件
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
-                // 此处定义的banner注释将插入到输出文件的顶部
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %> */\n'
+                //大括号包裹
+                curly: true,
+                //对于简单类型，使用===和!==，而不是==和!=
+                eqeqeq: true,
+                //对于首字母大写的函数（声明的类），强制使用new
+                newcap: true,
+                //禁用arguments.caller和arguments.callee
+                noarg: true,
+                //对于属性使用aaa.bbb而不是aaa['bbb']
+                sub: true,
+                //查找所有未定义变量
+                undef: true,
+                //查找类似与if(a = 0)这样的代码
+                boss: true,
+                //指定运行环境为node.js
+                node: true
             },
+            // globals: {
+            //     console: true,
+            //     define: true,
+            //     global: true,
+            //     module: true
+            // }
+        },
+        uglify: {
             build: {
                 files: [{
                     expand: true,
-                    cwd: 'src/js', //js目录下
-                    src: ['**/*.js', '!**/.svn', '!**/.idea', '!**/*.db', '!**/hash.js'], //所有js文件
-                    dest: 'dist/js' //输出到此目录下
+                    cwd: 'src/js',
+                    src: ['**/*.js'],
+                    dest: 'bin/dist/js'
                 }]
-            }
-        },
-        jshint: {
-            //define the files to lint
-            files: ['src/**/*.js'],
-            // configure JSHint (documented at http://www.jshint.com/docs/)
-            options: {
-                //这里是覆盖JSHint默认配置的选项
-                globals: {
-                    console: true,
-                    module: true
-                }
-            }
-        },
-        copy: {
-            js: {
-                cwd: 'src/js',
-                src: ['**', '!**/.svn', '!**/.idea', '!**/*.db', '!**/hash.js'],
-                dest: 'dist/js',
-                expand: true
-            },
-            img: {
-                cwd: 'src/images',
-                src: ['**', '!**/.svn', '!**/.idea', '!**/*.db'],
-                dest: 'dist/images',
-                expand: true
             }
         },
         less: {
@@ -47,23 +43,114 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'src/less',
-                    src: ['**/*.less', '!**/global.less'],
+                    src: ['**/*.less', '!**/global.less', '!**/mod-**.less'],
                     dest: 'src/css',
                     ext: '.css'
                 }]
             }
         },
         cssmin: {
-            with_banner: {
+            minify: {
+                expand: true,
+                cwd: 'src/css',
+                src: '**/*.css',
+                dest: 'bin/dist/css',
+                ext: ".css"
+            },
+            conbine: {
+                files: {
+                    'dist/css/base.min.css': ['bin/dist/css/*.css', '!bin/dist/css/base.css']
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'src/css',
-                    src: '**/*.css',
-                    dest: 'dist/css'
+                    cwd: 'src/images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'bin/dist/images'
                 }]
             }
         },
+        copy: {
+            js: {
+                expand: true,
+                cwd: 'bin/dist/js',
+                src: ['**'],
+                dest: 'dist/js'
+            },
+            css: {
+                expand: true,
+                cwd: 'bin/dist/css',
+                src: ['**'],
+                dest: 'dist/css'
+            },
+            img: {
+                expand: true,
+                cwd: 'bin/dist/images',
+                src: ['**'],
+                dest: 'dist/images'
+            },
+            font: {
+                expand: true,
+                cwd: 'src/font',
+                src: ['**'],
+                dest: 'dist/font'
+            }
+        },
+        concat: {
+            options: {
+                banner: '',
+                process: function (src, filepath) {
+                    return '//source: ' + filepath + '\n ' + src;
+
+                }
+            },
+            js: {
+                src: ['bin/dist/js/*.js'],
+                dest: 'dist/js/base.min.js'
+            },
+            css: {
+                src: ['bin/dist/css/*.css'],
+                dest: 'dist/css/base.min.css'
+            }
+        },
+        compress: {
+            pub:{
+                options: {
+                    archive: '<%= pkg.name%>-publish.zip'
+                },
+                files: [
+                    {expand: true, cwd: 'dist/', src: ['**'], dest: '<%= pkg.name%>/dist/'}, 
+                    {src: ['*.html'], dest: '<%= pkg.name%>/'}, 
+                    {expand: true, cwd: 'view/', src: ['**'], dest: '<%= pkg.name%>/view/'}, 
+                ]
+            },
+            pack:{
+                options: {
+                    archive: '<%= pkg.name%>-package.zip'
+                },
+                files: [
+                    {expand: true, cwd: 'dist/', src: ['**'], dest: '<%= pkg.name%>/dist/'}, 
+                    {expand: true, cwd: 'src/', src: ['**'], dest: '<%= pkg.name%>/src/'}, 
+                    {src: ['*.html'], dest: '<%= pkg.name%>/'}, 
+                    {expand: true, cwd: 'view/', src: ['**'], dest: '<%= pkg.name%>/view/'}, 
+                    {src: ['Gruntfile.js'], dest: '<%= pkg.name%>/'},
+                    {src: ['package.json'], dest: '<%= pkg.name%>/'} 
+                ]
+            }
+        },
+        clean: {
+            build: ['dest','bin','<%= pkg.name%>-publish.zip','<%= pkg.name%>-package.zip'],
+        },
         watch: {
+            options: {
+                dateFormat: function(time) {
+                    grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+                    grunt.log.writeln('Waiting for more changes...');
+                },
+            },
             js: {
                 files: ['src/js/*.js', '!**/.svn', '!**/.idea', '!**/*.db', '!**/hash.js'],
                 tasks: ['copy:js'],
@@ -72,48 +159,47 @@ module.exports = function(grunt) {
                 }
             },
             less: {
-                files: "src/less/*.less",
+                files: "src/less/**/*.less",
                 tasks: ['less:dev']
             },
             css: {
-                files: ['src/css/*.css'],
+                files: ['src/css/**/*.css'],
                 tasks: ['cssmin'],
                 options: {
                     spawn: false
                 }
             },
             image: {
-                files: ['src/images/*', '!**/.svn', '!**/.idea', '!**/*.db'],
+                files: ['src/images/**/*', '!**/.svn', '!**/.idea', '!**/*.db'],
                 tasks: ['copy:img'],
                 options: {
                     spawn: false
                 }
             }
-        },
-        concat: {
-            options: {
-                // 定义一个用于插入合并输出文件之间的字符
-                separator: ';'
-            },
-            dist: {
-                // 将要被合并的文件
-                src: ['dist/js/*.js','!dist/js/<%= pkg.name %>.js'],
-                // 合并后的JS文件的存放位置
-                dest: 'dist/js/<%= pkg.name %>.js'
-            }
         }
     });
-    // 加载提供任务的插件
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    //清理文件和文件夹
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    //检查js语法错误
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    //压缩js文件
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    //less文件编译到css
     grunt.loadNpmTasks('grunt-contrib-less');
+    //压缩css文件
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    //压缩image文件
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    //复制文件和文件夹
     grunt.loadNpmTasks('grunt-contrib-copy');
-    //grunt.loadNpmTasks('grunt-contrib-concat');
+    //合并文件
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    //运行预定义的任务只要监视的文件模式添加、更改或删除
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //压缩文件和文件夹
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-    //默认任务
-    grunt.registerTask('default', ['uglify', 'less:dev', 'cssmin', 'copy:img']);
-    grunt.registerTask('dev', ['copy:js', 'less:dev', 'cssmin', 'copy:img']);
-
+    grunt.registerTask('default', ['uglify', 'less:dev', 'cssmin:minify','imagemin', 'copy','clean']);
+    grunt.registerTask('pack',['compress:pack']);
+    grunt.registerTask('pub',['compress:pub']);
 };
